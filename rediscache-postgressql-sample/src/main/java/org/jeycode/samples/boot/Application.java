7216e108945d78922b15931b90fa1008d467a020
javi3code <j.code.initialize@gmail.com>
2023-06-05 19:19:17 +0200

    Add CD configuration

---
+++ rediscache-postgressql-sample/src/main/java/org/jeycode/samples/boot/Application.java
@@ -0,0 +1,13 @@
+package org.jeycode.samples.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class);
+  }
+
+}
